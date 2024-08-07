5a2e0d83ccb983c089d6d4edb63a71b4e616deda
Masi <51009658+MasiMasinga@users.noreply.github.com>
2024-08-07 02:56:31 +0200

    feat: add tweet crud logic

--- src/api/tweet/routes/tweet.js
+++ src/api/tweet/routes/tweet.js
@@ -4,6 +4,52 @@
  * tweet router
  */
 
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::tweet.tweet');
+module.exports = {
+    routes: [
+        {
+            method: 'POST',
+            path: '/tweet',
+            handler: 'tweet.create',
+            config: {
+                policies: [],
+                middlewares: [],
+            },
+        },
+        {
+            method: 'GET',
+            path: '/tweets',
+            handler: 'tweet.list',
+            config: {
+                policies: [],
+                middlewares: [],
+            },
+        },
+        {
+            method: 'GET',
+            path: '/tweet/:id',
+            handler: 'tweet.get',
+            config: {
+                policies: [],
+                middlewares: [],
+            },
+        },
+        {
+            method: 'PUT',
+            path: '/tweet/:id',
+            handler: 'tweet.update',
+            config: {
+                policies: [],
+                middlewares: [],
+            },
+        },
+        {
+            method: 'DELETE',
+            path: '/tweet/:id',
+            handler: 'tweet.delete',
+            config: {
+                policies: [],
+                middlewares: [],
+            },
+        },
+    ],
+};
--- src/api/tweet/services/tweet.js
+++ src/api/tweet/services/tweet.js
@@ -6,4 +6,74 @@
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::tweet.tweet');
+module.exports = createCoreService('api::tweet.tweet', ({ strapi }) => ({
+    
+    /**
+     * @param {{ tweet_text: any; tweet_url: any; }} data
+     */
+    async createTweet(data) {
+        const newTweet = await strapi.query('api::tweet.tweet').create({
+            data: {
+                tweet_text: data.tweet_text,
+                tweet_url: data.tweet_url,
+                publishedAt: new Date(),
+            }
+        });
+
+        return newTweet;
+    },
+
+
+    async getTweets() {
+        const tweets = await strapi.query('api::tweet.tweet').findMany();
+
+        return tweets;
+    },
+
+
+    /**
+     * @param {any} id
+     */
+    async getTweet(id) {
+        const tweet = await strapi.query('api::tweet.tweet').findOne({
+            where: {
+                id,
+            }
+        });
+
+        return tweet;
+    },
+
+   
+    /**
+     * @param {any} id
+     * @param {{ tweet_text: any; tweet_url: any; }} data
+     */
+    async updateTweet(id, data) {
+        const updatedTweet = await strapi.query('api::tweet.tweet').update({
+            where: {
+                id,
+            },
+            data: {
+                tweet_text: data.tweet_text,
+                tweet_url: data.tweet_url,
+            }
+        });
+
+        return updatedTweet;
+    },
+
+  
+    /**
+     * @param {any} id
+     */
+    async deleteTweet(id) {
+        const deletedTweet = await strapi.query('api::tweet.tweet').delete({
+            where: {
+                id,
+            }
+        });
+
+        return deletedTweet;
+    },
+}));
